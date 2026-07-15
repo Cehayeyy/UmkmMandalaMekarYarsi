5282742ba4178802a7d11ba56e69787b725ec0f3
dimas <dmspradita@gmail.com>
2026-07-15 17:13:57 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,50 @@
 <?php
 
+
+
 use Illuminate\Support\Facades\Route;
+
 use Inertia\Inertia;
 
+
+
 Route::get('/', function () {
+
     return Inertia::render('welcome');
+
 })->name('home');
 
+
+
 Route::get('/umkm', function () {
+
     return Inertia::render('umkm/umkmPage');
+
 })->name('umkm.umkmPage');
 
+
+
 Route::get('/produk', function () {
+
     return Inertia::render('produk');
+
 })->name('produk');
 
+
+
 Route::middleware(['auth'])->group(function () {
+
     Route::get('dashboard', function () {
+
         return Inertia::render('dashboard');
+
     })->name('dashboard');
+
 });
 
+
+
 require __DIR__.'/settings.php';
+
 require __DIR__.'/auth.php';
+
